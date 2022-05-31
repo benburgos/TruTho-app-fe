36e062c4fa6f6801537126c0bd3c31da3724247a
Ben <ben.a.burgos@gmail.com>
2022-05-31 15:02:29 -0700

    adds boilerplate header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,21 @@
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { Navbar } from 'react-bulma-components';
+
+function Header(props) {
+  return (
+    <Navbar>
+      <Navbar.Brand>
+        <Navbar.Item href="#">
+          TruTho
+        </Navbar.Item>
+      </Navbar.Brand>
+      <Navbar.Menu>
+        <Navbar.Container align="end">
+          <Navbar.Item href="#">At the end</Navbar.Item>
+        </Navbar.Container>
+      </Navbar.Menu>
+    </Navbar>
+  );
+}
+
+export default Header;
