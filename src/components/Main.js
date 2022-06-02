0c916d60c76beeae5d1a93e38ed43217dd0fde2f
Ben <ben.a.burgos@gmail.com>
2022-06-02 14:56:50 -0700

    adds create,update,delete functions

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
-import { Card, Media, Heading, Content } from 'react-bulma-components';
+import { Routes, Route } from 'react-router-dom';
+import Index from '../pages/Index';
+import Show from '../pages/Board';
 
 function Main(props) {
   const [board, setBoard] = useState(null);
@@ -11,58 +13,56 @@ function Main(props) {
     setBoard(data);
   }
 
-  // async function createBoard() {
-  //   await fetch(URL, {
-  //     method: 'POST',
-  //     headers: { 'Content-Type': 'Application/json' },
-  //     body: JSON.stringify(board),
-  //   });
-  //   getBoards();
-  // }
+  async function createBoard() {
+    await fetch(URL, {
+      method: 'POST',
+      headers: { 'Content-Type': 'Application/json' },
+      body: JSON.stringify(board),
+    });
+    getBoards();
+  }
 
-  // async function updateBoard() {
-  //   await fetch(URL, {
-  //     method: 'PUT',
-  //     headers: { 'Content-Type': 'Application/json' },
-  //     body: JSON.stringify(board),
-  //   });
-  //   getBoards();
-  // }
+  async function updateBoard() {
+    await fetch(URL, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'Application/json' },
+      body: JSON.stringify(board),
+    });
+    getBoards();
+  }
 
-  // async function deleteBoard() {
-  //   await fetch(URL + id, {
-  //     method: 'DELETE',
-  //   });
-  //   getPeople();
-  // }
-
-  const loaded = () => {
-    return board.map((board) => (
-      <Card key={board._id}>
-        <Card.Content>
-          <Media>
-            <Media.Item>
-              <Heading size={4}>{board.boardTitle}</Heading>
-              <time dateTime="2016-1-1">Created 12/31/22</time>
-            </Media.Item>
-          </Media>
-          <Content>
-            {board.list.length} Lists | {} Comments
-          </Content>
-        </Card.Content>
-      </Card>
-    ));
-  };
-
-  const loading = () => {
-    return <div className='loader'></div>;
-  };
+  async function deleteBoard() {
+    await fetch(URL + id, {
+      method: 'DELETE',
+    });
+    getPeople();
+  }
 
   useEffect(() => {
     getBoards();
   }, []);
 
-  return <main>{board ? loaded() : loading()}</main>;
+  return (
+    <main>
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={<Index board={board} createBoard={createBoard} />}
+        />
+        <Route
+          path="/boards/:id"
+          element={
+            <Show
+              board={board}
+              updateBoard={updateBoard}
+              deleteBoard={deleteBoard}
+            />
+          }
+        />
+      </Routes>
+    </main>
+  );
 }
 
 export default Main;
