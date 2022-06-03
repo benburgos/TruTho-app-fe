e732e2b021eeef70038d162869318f18e3315b5e
Ben <ben.a.burgos@gmail.com>
2022-06-02 18:59:12 -0700

    refactors code

--- src/pages/Board.js
+++ src/pages/Board.js
@@ -5,8 +5,22 @@ import { Card } from 'react-bulma-components';
 function Show({ board, updateBoard, deleteBoard }) {
   const { id } = useParams();
   const thisBoard = board.find((board) => board._id === id);
-  const thisList = thisBoard.find((list) => list._id)
   let navigate = useNavigate();
+
+  const [editForm, setEditForm] = useState(thisBoard);
+
+  const handleChange = (event) => {
+    setEditForm((prevState) => ({
+      ...prevState,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    updateBoard(editForm, thisBoard._id);
+    navigate('/');
+  };
 
   const removeBoard = () => {
     deleteBoard(thisBoard._id);
@@ -18,18 +32,22 @@ function Show({ board, updateBoard, deleteBoard }) {
       <h2>{thisBoard.boardTitle}</h2>
       <div className="button-container">
         <input type="submit" value="Edit Board"></input>
-        <input type="submit" value="Delete Board" onClick={removeBoard}></input>
+        <input type="submit" value="Delete Board"></input>
       </div>
       <div className="showList">
-        {thisBoard.list.map((list) => (
-          <div key={list._id} className="showList-item">
+        {thisBoard.list.map((list, index) => (
+          <div key={index} className="showList-item">
             <h3>{list.listTitle}</h3>
             <hr />
-            {list.listCards.map((comments) => (
-              <div key={comments._id} className="showList-comment">
+            {list.listCards.map((comments, index) => (
+              <div key={index} className="showList-comment">
                 <h4>{comments.cardComment}</h4>
               </div>
             ))}
+            <form>
+              <input type="text" name="cardComment" />
+              <button>Submit</button>
+            </form>
           </div>
         ))}
       </div>
