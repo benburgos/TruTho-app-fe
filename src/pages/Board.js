1b954f6bee2415a97a041353e01ba129f34c5cc2
Ben <ben.a.burgos@gmail.com>
2022-06-02 19:33:22 -0700

    writes test code

--- src/pages/Board.js
+++ src/pages/Board.js
@@ -7,21 +7,6 @@ function Show({ board, updateBoard, deleteBoard }) {
   const thisBoard = board.find((board) => board._id === id);
   let navigate = useNavigate();
 
-  const [editForm, setEditForm] = useState(thisBoard);
-
-  const handleChange = (event) => {
-    setEditForm((prevState) => ({
-      ...prevState,
-      [event.target.name]: event.target.value,
-    }));
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    updateBoard(editForm, thisBoard._id);
-    navigate('/');
-  };
-
   const removeBoard = () => {
     deleteBoard(thisBoard._id);
     navigate('/');
@@ -31,8 +16,8 @@ function Show({ board, updateBoard, deleteBoard }) {
     <Card className="showBoard">
       <h2>{thisBoard.boardTitle}</h2>
       <div className="button-container">
-        <input type="submit" value="Edit Board"></input>
-        <input type="submit" value="Delete Board"></input>
+        <input type="submit" value="Edit Board" />
+        <input type="submit" value="Delete Board" onClick={removeBoard}></input>
       </div>
       <div className="showList">
         {thisBoard.list.map((list, index) => (
@@ -44,10 +29,6 @@ function Show({ board, updateBoard, deleteBoard }) {
                 <h4>{comments.cardComment}</h4>
               </div>
             ))}
-            <form>
-              <input type="text" name="cardComment" />
-              <button>Submit</button>
-            </form>
           </div>
         ))}
       </div>
